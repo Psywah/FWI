c27df99d1d9a603f9701d70ecb3db7c3e2138f29
ShaunaghDowning <shaunagh.downing@gmail.com>
2019-08-21 15:40:00 +0100

    Update Example1.m

--- FWIMatlab/Examples/Example1.m
+++ FWIMatlab/Examples/Example1.m
@@ -53,9 +53,6 @@ fwimaxit=100;
 
 %% Regularisation
 alpha=0;       % FWI regularisation parameter
-beta=0;        % Sensor optimisation regularisation parameter 
-regp=0;        % regularisation position
-
 
 %% FWI
 
@@ -128,77 +125,4 @@ ylabel('z (km)','fontsize',12);
 
 
 
-%% Sensor Optimisation
- 
-% Starting guess for sensor position  
-%p0=model.zr;
 
-% % BB optimisation
-%   [pk,hist, error, mw, pvec] = BBOptimOnePixel(p0,1e-12,100,m,model, alpha, m0, beta, regp, fwitol, fwimaxit, mtrue);
- 
-%  disp(['Optimum sensor location, when x = ' num2str(x(1)) ' is z = ' num2str(pk)])
-%  disp(['\psi at this position = ' num2str(hist(end,2))])
- 
-% %final velocity model
-%  vw = reshape(real(1./sqrt(mw)),n);
-% %Ground truth model
-% vtrue = reshape(real(1./sqrt(mtrue)),n);
-
-% % %Plotting
-
-% %GT
-%  figure;
-%  imagesc(1e-3*x,1e-3*z,v)
-%  axis equal tight
-%  title('Ground Truth'); 
-%  xlabel('x [km]','fontsize',20);
-%  ylabel('z [km]','fontsize',20);
-
-
-% %Noisy model  
-% % % figure;
-% % %  imagesc(1e-3*x,1e-3*z,vnoise)
-% % %  axis equal tight
-% % %  title('Nosiy Model'); 
-% % %  xlabel('x [km]','fontsize',20);
-% % %  ylabel('z [km]','fontsize',20);
-
-% %Initial 
-% %  figure;
-% %  imagesc(1e-3*x,1e-3*z,v0)
-% %  axis equal tight
-% %  title('Initial Velocity');
-% %  xlabel('x [km]','fontsize',20);
-% %  ylabel('z [km]','fontsize',20);
-%   hold on
-%   plot(model.xr*1e-3,model.zr*1e-3, 'o-','MarkerFaceColor','red','MarkerEdgeColor','red')
-%  plot(model.xs*1e-3,model.zs*1e-3, 'o','MarkerFaceColor','green','MarkerEdgeColor','green')
-   
-% %Optimised 
-%  figure;
-%  imagesc(1e-3*x,1e-3*z,vw)
-%  axis equal tight
-%  title('Optimised Sensor Position'); 
-%  xlabel('x [km]','fontsize',20);
-%  ylabel('z [km]','fontsize',20);
-%  hold on 
-% plot(model.xs*1e-3,model.zs*1e-3, 'o','MarkerFaceColor','green','MarkerEdgeColor','green', 'MarkerSize', 8)
-%  plot(model.xr*1e-3,p0*1e-3, '*','MarkerFaceColor','red','MarkerEdgeColor','red')
-% plot(model.xr*1e-3,pk*1e-3, 'o','MarkerFaceColor','red','MarkerEdgeColor','red', 'MarkerSize', 5) 
-% legend('Source', 'Initial Sensor Positions','Optimised Sensor Position', 'Location', 'EastOutside')
-%  
- 
-% %Upper misfit  
-% figure
-% plot(hist(:,1), hist(:,2)/hist(1,2), 'linewidth', 2)
-% xlabel('Iteration Number', 'fontsize', 14)
-% ylabel('f(x)=1/2||m_{true}-m_w||^2', 'fontsize', 14)
-% title('Function Value', 'fontsize', 16)
-% 
-
-% %Upper gradient
-% figure
-% plot(hist(:,1), hist(:,3)/hist(1,3), 'linewidth', 2)
-% xlabel('Iteration Number', 'fontsize', 14)
-% ylabel('||g(x)||_2', 'fontsize', 14)
-% title('Norm of Gradient', 'fontsize', 16)
